9f11ff632995cd89fafa90d6761033e0ebe26beb
shwethak <shwethakstar@gmail.com>
2016-12-20 13:46:15 +0530

    finally

--- flaskr.py
+++ flaskr.py
@@ -50,3 +50,21 @@ def initdb_command():
     """Initializes the database."""
     init_db()
     print 'Initialized the database.'
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
